eab525b40a2bcc321e4e8807e9abe6babd1f9f5a
Wren Powell <wrenp@duck.com>
2023-07-17 12:55:39 -0400

    Avoid printing a stack trace for user-facing errors

--- src/error.rs
+++ src/error.rs
@@ -2,21 +2,21 @@ use std::path::PathBuf;
 
 use thiserror::Error;
 
-/// A error type for "expected" errors.
+/// A error type for user-facing errors.
 ///
 /// This type represents errors expected in common usage of the program that should trigger a
 /// readable error message instead of a stack trace.
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("This option name is invalid: {name}")]
+    #[error("This is not a valid option name: `{name}`.\n\nSee the sources.list(5) man page for a list of valid options or use `--force-literal-options`.")]
     InvalidOptionName { name: String },
 
-    #[error("This option is not in `key=value` format: {option}")]
+    #[error("This option is not in `key=value` format: `{option}`.")]
     MalformedOption { option: String },
 
-    #[error("This source file already exists: {path}")]
+    #[error("This source file already exists: `{path}`.\n\nYou can either overwrite it with `--overwrite` or pick a different name for the source.")]
     SourceFileAlreadyExists { path: PathBuf },
 
-    #[error("You must run this command as root")]
+    #[error("You must run this command as root.")]
     PermissionDenied,
 }
--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,26 @@ mod keyring;
 mod option;
 mod source;
 
+use std::process::ExitCode;
+
 use clap::Parser;
-
 use cli::Cli;
+use error::Error;
 
-fn main() -> eyre::Result<()> {
+fn main() -> eyre::Result<ExitCode> {
     color_eyre::install()?;
 
     let cli = Cli::parse();
 
-    cli.command.dispatch()?;
+    if let Err(err) = cli.command.dispatch() {
+        // User-facing errors should not show a stack trace.
+        if let Some(user_facing_err) = err.downcast_ref::<Error>() {
+            eprintln!("{}", user_facing_err);
+            return Ok(ExitCode::FAILURE);
+        }
 
-    Ok(())
+        return Err(err);
+    }
+
+    Ok(ExitCode::SUCCESS)
 }
