02f6affc795fe6a938d8341c0663365eb28c27d6
Wren Powell <wrenp@duck.com>
2023-07-22 11:13:36 -0400

    Implement logic for backup up original source file

--- src/convert.rs
+++ src/convert.rs
@@ -1,8 +1,12 @@
+use std::borrow::Cow;
+use std::fs;
+use std::io;
 use std::path::PathBuf;
 
 use eyre::{bail, WrapErr};
 
 use crate::cli::{BackupArgs, Convert, ConvertDestArgs};
+use crate::error::Error;
 use crate::file::{SourceFile, SourceFileKind, SourceFilePath};
 use crate::option::OptionMap;
 use crate::parse::parse_line_file;
@@ -27,11 +31,12 @@ impl BackupMode {
     }
 }
 
-/// A converter for converting a repo source file.
+/// A converter for converting a repo source file from the single-line syntax to the deb822 syntax.
 #[derive(Debug)]
 pub struct EntryConverter {
     options: Vec<OptionMap>,
     backup_mode: Option<BackupMode>,
+    in_file: SourceFile,
     out_file: SourceFile,
 }
 
@@ -80,20 +85,58 @@ impl ConvertDestArgs {
 }
 
 impl EntryConverter {
+    const BAKCKUP_EXT: &str = ".bak";
+
     /// Construct an instance from CLI args.
     pub fn from_args(args: &Convert) -> eyre::Result<Self> {
         let in_file = args.dest.in_file()?;
         let out_file = args.dest.out_file()?;
 
-        let options =
-            parse_line_file(&in_file.path()).wrap_err("failed parsing source entry file")?;
+        let in_path = in_file.path();
+
+        let options = match parse_line_file(&in_path) {
+            Ok(options) => options,
+            Err(err) => match err.downcast_ref::<io::Error>() {
+                Some(io_err) if io_err.kind() == io::ErrorKind::NotFound => {
+                    bail!(Error::SourceFileNotFound {
+                        path: in_path.into_owned(),
+                    })
+                }
+                _ => bail!(err.wrap_err("failed to parse original source file")),
+            },
+        };
 
         let backup_mode = BackupMode::from_args(&args.backup);
 
         Ok(EntryConverter {
             options,
             backup_mode,
+            in_file,
             out_file,
         })
+    }
+
+    /// Backup the original source file.
+    fn backup_original(&self) -> eyre::Result<()> {
+        let in_path = self.in_file.path();
+
+        let backup_path = match &self.backup_mode {
+            Some(BackupMode::Backup) => Cow::Owned(in_path.join(Self::BAKCKUP_EXT)),
+            Some(BackupMode::BackupTo { path }) => Cow::Borrowed(path),
+            None => return Ok(()),
+        };
+
+        fs::copy(&in_path, backup_path.as_ref())
+            .wrap_err("failed copying source file to backup path")?;
+
+        Ok(())
+    }
+
+    /// Convert the source entry.
+    pub fn convert(&self) -> eyre::Result<()> {
+        self.backup_original()
+            .wrap_err("failed to create backup of original source file")?;
+
+        todo!()
     }
 }
